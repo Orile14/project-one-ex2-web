ed20620eb654555976d02e920adf329669a7131c
Eitan <eitanmaimoni1@gmail.com>
2024-03-05 19:57:16 +0200

    styling

--- src/optionSection/optionItem/optionItem.js
+++ src/optionSection/optionItem/optionItem.js
@@ -10,8 +10,8 @@ const OptionItem = ({ iconClass, onLogout, deleteUser}) => {
                     <button type="button" id ="logOutBtn" className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                     </button>
                     <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#" onClick={onLogout}>logout</a></li>
-                        <li><a className="dropdown-item" href="#" onClick={deleteUser} >DELETE user</a></li>
+                        <li><a className="dropdown-item" href="#" onClick={onLogout}>Logout</a></li>
+                        <li><a className="dropdown-item" href="#" onClick={deleteUser} >Delete user</a></li>
                     </ul>
                 </div>
             ) : (
